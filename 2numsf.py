10316dc486cc65343695c3b939f19ba69333c5e1
Namrata040 <nandedkarnamrata2@gmail.com>
2024-07-24 16:44:48 +0530

    Add files via upload

---
+++ 2numsf.py
@@ -0,0 +1,11 @@
+ import numpy as np
+
+# Define the input array
+arr = np.array([20, 2, 7, 1, 34])
+
+# Calculate the standard deviation
+std_deviation = np.std(arr)
+
+# Print the standard deviation
+print("Standard deviation of the array:", std_deviation)
+ Standard deviation of the array: 14.789319726725126
